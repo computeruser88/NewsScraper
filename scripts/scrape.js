d9f4dcafdf9a53b50df1eb72471b6b699904a0bc
Jan Balangue <jantonio.balangue@gmail.com>
2018-06-15 13:56:14 -0700

    Modify scrape.js

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -6,17 +6,16 @@ const scrape = function(callback) {
     request("http://feeds.washingtonpost.com/rss/national"), function(err, res, body) {
         const $ = cheerio.load(body);
         let articles = [];
-        let dataToAdd = [];
 
         $(".item").each(function(index, element) {
             const headline = $(this).children(".item .header a").text().trim();
             const sum = $(this).children(".item div").text().trim();
             if (head && summary) {
-                const dataToAdd[index] = {
+                const dataToAdd = {
                     title: headline,
                     summary: sum
                 };
-            articles.push(dataToAdd[index]);
+            articles.push(dataToAdd);
             }
         });
         callback(articles);
