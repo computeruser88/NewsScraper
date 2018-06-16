0d13935774f737ce33e71cdd3139e48a53a937f9
Jan Balangue <jantonio.balangue@gmail.com>
2018-06-15 20:26:18 -0700

    Modify index.js and saved.js

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -17,7 +17,7 @@ $(document).on("click", "p", function () {
             $("#notes").append("<h2>" + data.title + "</h2>");
             $("#notes").append("<input id='titleinput' name='title' >");
             $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
-            $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+            $("#notes").append("<button data-id='" + data._id + "' class='btn note-save' id='savenote'>Save Note</button>");
 
             if (data.note) {
                 $("#titleinput").val(data.note.title);
